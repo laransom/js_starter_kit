b9cdb05f0bdea1e5489f658998355ad13555fba7
laransom <laransom@gmail.com>
2014-04-29 05:16:22 -0400

    changed to jquery

--- js/app.js
+++ js/app.js
@@ -1,3 +1,55 @@
+
+var randomnumber=Math.floor(Math.random()*11);
+
+var $textarea = $('#user_guess placeholder');
+$textarea.attr('placeholder', 'hello world');
+
+document.getElementById('user_guess').focus();
+
+$(document).ready(function() {
+  $( "#submit" ).click(function() {
+    guess = $('#user_guess').val();
+    compare(guess, randomnumber);
+  });
+});
+
+$(document).keypress(function(e) {
+  if(e.which == 13 ) {
+    guess = $('#user_guess').val();
+    compare(guess, randomnumber);
+    $('#user_guess').focus();
+  }
+});
+
+var compare = function (guess, number) {
+  if ( guess < randomnumber ) {
+      $( ".alert-box" ).text("Too low. Guess a higher number");
+  } else if (guess > randomnumber) {
+      $( ".alert-box" ).text("Too high. Guess a lower number");
+  } else {
+      $( ".alert-box" ).text("You win!");
+  }
+};
+/*
+  while ( guess != randomnumber ) {
+    if ( guess < randomnumber ) {
+      $( ".alert_box" ).text("Too low. Guess a higher number");
+    } else if (guess > randomnumber) {
+      $( ".alert_box" ).text("Too high. Guess a lower number");
+    }
+  };
+
+
+
+
+  $( ".alert-box" ).text($(this).val());
+
+
+
+
+
+<input type="text" placeholder="Put guess here">
+
 var randomnumber=Math.floor(Math.random()*11);
 
 username = prompt("What is your name?");
@@ -14,3 +66,4 @@ while ( guess != randomnumber ) {
 }
 
 alert(username + ", you sir are a winner!!");
+*/
